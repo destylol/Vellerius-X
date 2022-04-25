631bdf0620085e4d0c1c5a1bed83354445e4aaa5
corrupt <98520172+Corrupt2625@users.noreply.github.com>
2022-04-25 18:41:26 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,12 +1,5 @@
-if game.PlaceId == 2753915549 then
-    print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
-    print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
-        print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
-    print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
-        print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")        print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
-        print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")    print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
- print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
- print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")    print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad") print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad") print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad") print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
-      print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")  print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad") loadstring(game:HttpGet("https://raw.githubusercontent.com/Corrupt2625/Bloxfruits/main/Main.lua"))() print("wadhsahdwgyagdsgaydhwuhduigsasfdegweyfgyuhuhdasgdwhudgyusaggggggggwgadyyuasssssssssddddddwad")
+if game.PlaceId = 9049840490 then
+    loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\39\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\67\111\114\114\117\112\116\50\54\50\53\47\86\101\108\108\101\114\105\117\115\45\88\47\109\97\105\110\47\83\111\110\105\99\83\112\101\101\100\83\105\109\46\108\117\97\39\41\41\40\41")()
+elseif game.PlaceId = 3956818381 then
     
 end
